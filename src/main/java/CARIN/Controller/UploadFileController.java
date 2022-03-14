e600ce32b8f6a35574275a82c1ccc945418ef6ca
Suparida <jik2002x@gmail.com>
2022-03-14 19:01:03 +0700

    add Genetic file input handler

--- src/main/java/CARIN/Controller/UploadFileController.java
+++ src/main/java/CARIN/Controller/UploadFileController.java
@@ -8,19 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
-public class UploadfileController {
+public class UploadFileController {
 
     @RequestMapping("/uploadfile")
     public void upload(@RequestParam String file ,@RequestParam int number) throws IOException {
          try {
              GeneticManager geneticManager = GeneticManager.createGeneticManager();
              // if number is 1-3 then set antibody genetic code
-             if(number <= 3)
-                 geneticManager.setAntibody(number, file);
+             if(number <= 3) {
+                 geneticManager.setAntibody(number - 1, file);
+//                 System.out.println("num:" + (number - 1) + " = " + file);
                  // if number is 4-6 then set virus genetic code
-             else if(number <=6)
-                 geneticManager.setVirus(number, file);
-             else
+             }else if(number <=6) {
+                 geneticManager.setVirus(number - 4, file);
+//                 System.out.println("num:" + (number - 1) + " = " + file);
+             }else
                  throw new IllegalArgumentException();
 
          }catch (IllegalArgumentException e){
