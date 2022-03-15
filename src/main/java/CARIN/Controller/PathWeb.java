3d5c6a672482a303fc37a9259e2e0be56b118e1d
Siriwat Songwattana <pan-191260@hotmail.com>
2022-03-15 14:17:28 +0700

    fix controller

--- src/main/java/CARIN/Controller/PathWeb.java
+++ src/main/java/CARIN/Controller/PathWeb.java
@@ -2,7 +2,6 @@ package CARIN.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class PathWeb {
--- src/main/java/CARIN/Controller/StartController.java
+++ src/main/java/CARIN/Controller/StartController.java
@@ -1,13 +1,9 @@
 package CARIN.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StartController {
-    @RequestMapping("/")
-    public void start(){
-        
-    }
     
 }
