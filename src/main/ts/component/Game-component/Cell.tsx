ddbb151c562ae5a3f0213fba70b30912c6181413
lalinthorn <lalinpye@gmail.com>
2022-03-16 01:21:41 +0700

    avoid paste antibody at the same cell

--- src/main/ts/component/Game-component/Cell.tsx
+++ src/main/ts/component/Game-component/Cell.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CellStore, selectcell } from "../../stores/Cellstore";
+import { CellStore, selectcell, selectanti } from "../../stores/Cellstore";
 import block from '../CSSstyle/image/block.png';
 import anti1 from '../CSSstyle/image/anti1.png'
 import anti2 from '../CSSstyle/image/anti2.png'
@@ -35,7 +35,10 @@ const fetch_order_location = (a: number, b: number,  type:String , order: number
 const size = () => {
   const state = CellStore.useState()
   if (state.low < 6 && state.colum < 12) return "w-32 h-32 p-4"
-  else return "w-24 h-24 p-4"
+  else if(state.low<7 && state.colum<16) return "w-24 h-24 p-4"
+  else if(state.low<9 && state.colum<19) return "w-20 h-20 p-3.5"
+  else if(state.low<12 && state.colum<21) return "w-14 h-14 p-3"
+  else return  "w-11 h-11 p-2.5"
 }
 
 const Cell = ({ x, y }: celltype) => {
@@ -45,9 +48,12 @@ const Cell = ({ x, y }: celltype) => {
   
   return (
     <td className={`${size()} cursor-pointer`} style={{ borderColor: "transparent", backgroundImage: `url(${block})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover' }} onClick={() => {
+      if(state.log[y][x] === ''){
       order = order + 1
       selectcell(y, x, state.host)
+      selectanti('')
       fetch_order_location(y, x, state.host , order )
+      }
     }}>
       {render(state.log[y][x])}
     </td>
--- src/main/ts/component/Game.tsx
+++ src/main/ts/component/Game.tsx
@@ -3,7 +3,7 @@ import Body from "./Game-component/body";
 const Game = () => {
 
   return (
-    <div style={{ backgroundColor: "#FFD7B5" }}>
+    <div className=" bg-body bg-center bg-no-repeat  w-full h-screen" style={{ backgroundColor: "#FFD7B5" }}>
       <div className="my-auto space-y-3 ">
         <Body/>
       </div>
