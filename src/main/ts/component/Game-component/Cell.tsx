aeda2c99ae788ab71bb10181d64272bf63fa38f7
lalinthorn <lalinpye@gmail.com>
2022-03-17 03:08:04 +0700

    fix onclik move

--- src/main/ts/component/Game-component/Cell.tsx
+++ src/main/ts/component/Game-component/Cell.tsx
@@ -17,15 +17,15 @@ const render = (host: string) => {
   const state = CellStore.useState();
   if (host == 'anti1')
     return (
-      <img src={anti1} onClick={()=>{state.host = "anti1"}}></img>
+      <img src={anti1} onClick={()=>{selectanti('anti1')}}></img>
     )
   if (host == 'anti2')
     return (
-      <img src={anti2} onClick={()=>{state.host = "anti2"}}></img>
+      <img src={anti2} onClick={()=>{selectanti('anti2')}}></img>
     )
   if (host == 'anti3')
     return (
-      <img src={anti3} onClick={()=>{state.host = "anti3"}}></img>
+      <img src={anti3} onClick={()=>{selectanti('anti3')}}></img>
     )
     if (host == 'virus1')
     return (
